7de99c765b6ba289f4eba0f1e78a48eccf52d4a3
João Markus <markusjoaomarkus99@gmail.com>
2025-04-21 07:03:25 -0300

    chore(prod): ativando deploy automatizado para produção

---
+++ src/main/java/br/com/fiap/transito/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package br.com.fiap.transito.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
+
